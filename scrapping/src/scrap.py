e2da2fc8f8dedf18c2761a287d9b8f63eb86bf15
oyindolapo <oyindolapo.komolafe@monetha.io>
2021-07-11 21:48:03 +0300

    Enum and Pagination function of scrap

---
+++ scrapping/src/scrap.py
@@ -0,0 +1,125 @@
+from enum import Enum
+from typing import List
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+class StackTab(str, Enum):
+    """
+        Enum of all question tabs for Stackoverflow. This ensures that the user doesnt have to provide URL for scrapping.
+    """
+    NEW = "https://stackoverflow.com/questions?tab=Newest&pagesize=50"
+    ACTIVE = "https://stackoverflow.com/questions?tab=Active&pagesize=50"
+    BOUNTIFIED = "https://stackoverflow.com/questions?tab=Bounties&pagesize=50"
+    UNANSWERED = "https://stackoverflow.com/questions?tab=Unanswered&pagesize=50"
+    FREQUENT = "https://stackoverflow.com/questions?tab=Frequent&pagesize=50"
+    VOTES = "https://stackoverflow.com/questions?tab=Votes&pagesize=50"
+
+class ScrapStackOverflow:
+    """
+        A class used to scrap StackOverflow website for questions
+        The filter for scrapping is Recent Activity
+        ...
+
+        Attributes
+        ----------
+        scrapped_url : str
+            the url that is scrapped
+        name : str
+            the name of the animal
+        sound : str
+            the sound that the animal makes
+        num_legs : int
+            the number of legs the animal has (default 4)
+
+        Methods
+        -------
+        says(sound=None)
+            Prints the animals name and what sound it makes
+    """
+
+    def __init__(self, tab: Enum = StackTab.ACTIVE) -> None:
+        """
+            Parameters
+            ----------
+            tab : Enum
+                The Enum of the question tab to be scrapped. Default is StackTab.ACTIVE
+
+            Raises
+            ------
+            TypeError
+                If no sound is set for the animal or passed in as a
+                parameter.
+            
+        """
+        if not isinstance(tab, StackTab):
+            raise TypeError("tab must be an instance of StackTab Enum")
+
+        self.__url = tab.value
+
+    @property
+    def scrapped_url(self) -> str:
+        """
+            Returns the scrapped url
+        """
+        return self.__url
+
+    def scrap(self, num_of_samples:int = 10) -> pd.DataFrame:
+        """Scraps the requested .
+
+        Scraps the url and returns a dataframe of size num_of_samples
+
+        Parameters
+        ----------
+        num_of_samples : int
+            The number of samples to be collected from the url. Default is 10
+
+        Raises
+        ------
+        TypeError
+            If num of samples isnt int, TypeError is raised.
+
+        Returns
+        -------
+        list
+            a Dataframe of scrapped items
+        """
+        if not isinstance(num_of_samples, int):
+            raise TypeError("integer expected for num_of_samples")
+
+        items = []
+        page_count = round(num_of_samples/50) if round(num_of_samples % 100) == 0 else round(num_of_samples/ 100) + 1
+        self.__combinedProcessing(page_count)
+
+        return pd.DataFrame(items)
+
+    def  __combinedProcessing(self, page_count) -> None:
+        """
+            Run the processing
+        """
+        for num in range(0, page_count):
+            url = f"{self.__url}&page={num}"
+            page_content = self.__get_page_content(url)
+            self.__scrap_content(page_content[1])
+            #print(type(page_content))
+            print(type(page_content[1]))
+
+    def __get_page_content(self, url: str) -> tuple:
+        headers = {
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.54',
+        'accept': 't	text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.stackoverflow.com/',
+        'accept-language': 'en-US,en;q=0.9',
+        }
+        read_site = requests.get(url, headers=headers)
+        return read_site.status_code, read_site.content
+
+    def __scrap_content(self, content) -> List:
+        pass
+
+ScrapStackOverflow(StackTab.ACTIVE).scrap(10)
