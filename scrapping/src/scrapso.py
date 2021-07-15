c09cdc62bfb85a183e089e9797d49e91b063bd56
oyindolapo <oyindolapo.komolafe@monetha.io>
2021-07-15 06:34:34 +0300

    Packaged StackOverflow SCrapping

--- scrapping/src/scrapso.py
+++ scrapping/src/scrapso.py
@@ -15,6 +15,7 @@ class StackTab(str, Enum):
     FREQUENT = "https://stackoverflow.com/questions?tab=Frequent&pagesize=50"
     VOTES = "https://stackoverflow.com/questions?tab=Votes&pagesize=50"
 
+
 class ScrapStackOverflow:
     """
         A class used to scrap StackOverflow website for questions
@@ -25,17 +26,11 @@ class ScrapStackOverflow:
         ----------
         scrapped_url : str
             the url that is scrapped
-        name : str
-            the name of the animal
-        sound : str
-            the sound that the animal makes
-        num_legs : int
-            the number of legs the animal has (default 4)
-
+        
         Methods
         -------
-        says(sound=None)
-            Prints the animals name and what sound it makes
+        scrap(num_of_samples:int = 10)
+            Returns the scrapped information
     """
 
     def __init__(self, tab: Enum = StackTab.ACTIVE) -> None:
@@ -64,39 +59,46 @@ class ScrapStackOverflow:
         """
         return self.__url
 
-    def scrap(self, num_of_samples:int = 10) -> pd.DataFrame:
-        """Scraps the requested .
+    def scrap(self, num_of_samples:int = 10, pagestart:int = 1) -> pd.DataFrame:
 
-        Scraps the url and returns a dataframe of size num_of_samples
+        """
+            Scraps the requested .
 
-        Parameters
-        ----------
-        num_of_samples : int
-            The number of samples to be collected from the url. Default is 10
+            Scraps the url and returns a dataframe of size num_of_samples
 
-        Raises
-        ------
-        TypeError
-            If num of samples isnt int, TypeError is raised.
+            Parameters
+            ----------
+            num_of_samples : int
+                The number of samples to be collected from the url. Default is 10
 
-        Returns
-        -------
-        list
-            a Dataframe of scrapped items
+            Raises
+            ------
+            TypeError
+                If num of samples isnt int, TypeError is raised.
+
+            Returns
+            -------
+            list
+                a Dataframe of scrapped items
         """
         if not isinstance(num_of_samples, int):
             raise TypeError("integer expected for num_of_samples")
 
-        page_count = round(num_of_samples/50) if round(num_of_samples % 100) == 0 else round(num_of_samples/ 100) + 1
-        self.__combinedProcessing(page_count, num_of_samples)
+        if not isinstance(pagestart, int):
+            raise TypeError("integer expected for pagestart")
+        if pagestart == 0:
+            page_count = round(num_of_samples/50) if round(num_of_samples % 100) == 0 else round(num_of_samples/ 100) + 1
+        else: 
+            page_count = pagestart + (round(num_of_samples/50) if round(num_of_samples % 100) == 0 else round(num_of_samples/ 100) + 1)
+        self.__combinedProcessing(page_count, num_of_samples, pagestart)
 
         return pd.DataFrame(self.__items)
 
-    def  __combinedProcessing(self, page_count, num_of_samples) -> None:
+    def  __combinedProcessing(self, page_count, num_of_samples, pagestart) -> None:
         """
             Run the processing
         """
-        for num in range(0, page_count):
+        for num in range(pagestart, page_count):
             url = f"{self.__url}&page={num}"
             page_content = self.__get_page_content(url)
             self.__scrap_content(page_content[1], num_of_samples)
@@ -132,15 +134,24 @@ class ScrapStackOverflow:
         """
         soup = BeautifulSoup(content, "html.parser")
         for info in soup.find_all(class_ = "question-summary"):
-            tags = ""
+            tags = badge = reputation = ""
+            answer = 0
             for tag in info.find_all("a", "post-tag flex--item"):
                 tags = f"{tags} : {tag.text}"
 
-
             user = user_url =  ""
-            for usr in info.find_all("div", "user-details"):
-                user = usr.a.text
-                user_url = usr.a['href']
+            if info.find("div", "user-details"):
+                user = info.find("a").text
+                user_url = info.find("a")['href']
+
+            if info.find("span", "reputation-score"):
+                reputation = info.find("span", "reputation-score").text
+
+            if info.find_all("div", {"class": "answered-accepted"}):
+                answer = 1
+
+            if info.find("span", "v-visible-sr"):
+                badge = info.find("span", "v-visible-sr").text
 
             self.__items.append(
                 {
@@ -152,14 +163,15 @@ class ScrapStackOverflow:
                     'question_time': info.find("span", "relativetime").attrs['title'], 
                     'user': user,  
                     'user_url': user_url,
-                    'user_badge': info.find("span", "v-visible-sr").text, 
-                    'user_reputation_score': info.find("span", "reputation-score").text, 
+                    'user_badge': badge, 
+                    'user_reputation_score': reputation, 
                     'votes': info.find("span", "vote-count-post").text,
                     'answer': info.find("div", "status").text,
-                    'views': info.find("div", "views").text
+                    'views': info.find("div", "views").text,
+                    'accepted_answer' : answer
                 }
             )
+            print(f"scraped {len(self.__items)} of {num_of_samples}")
             if len(self.__items) >= num_of_samples:
                 break
-
 
